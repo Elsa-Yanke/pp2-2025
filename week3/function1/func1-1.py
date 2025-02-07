417c53d8f1fb93f8af04f5772410b6e236e2a39b
Elsa Yanke <tokoaymi@gmail.com>
2025-02-07 21:53:06 +0500

    convert to grams

--- week3/function1/func1-1.py
+++ week3/function1/func1-1.py
@@ -2,5 +2,6 @@ def convertounces(grams):
     ounces = 28.3495231 * grams
     print(ounces)
 
-grams = int(input())
-convertounces(grams)
+if __name__ == "__main__":
+    grams = int(input())
+    print(convertounces(grams))
