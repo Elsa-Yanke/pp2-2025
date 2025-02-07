82eebc6380660455560b54b470f553e18c4c0bd2
Elsa Yanke <tokoaymi@gmail.com>
2025-02-07 22:32:23 +0500

    reverse sentence

--- week3/function1/reverse.py
+++ week3/function1/reverse.py
@@ -1,10 +1,10 @@
 def reverse(s):
     w = s.split()  
-    l = w[::-1]
-    return ' '.join(l)
+    w.reverse()
+    print(" ".join(w))
 
 
 if __name__ == "__main__":
     r = input()
-    print(reverse(r))
+    reverse(r)
             
