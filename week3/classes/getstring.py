0a3bdfd2a260cead2518b22eb36ba9128275a05f
Elsa Yanke <tokoaymi@gmail.com>
2025-02-07 21:02:30 +0500

    to upper case string

--- week3/classes/getstring.py
+++ week3/classes/getstring.py
@@ -1,20 +1,8 @@
-class Account:
-    def __init__(self,owner,balance):
-        self.owner = owner
-        self.balance = balance
-
-    def deposit(self, amount):
-        self.balance +=amount
-        print(f"{amount}$ accepted, new balance {self.balance}$")
-    def withdraw(self, amount):
-        if amount > self.balance:
-            print("Denied, you must have more money")
-        else:
-            self.balance -=amount
-            print(f"{amount}$ withdrawed, balance {self.balance}$")
-acc = Account('Anna', 1500)
-
-acc.deposit(500)
-acc.withdraw(400)
-acc.withdraw(15000)
-acc.withdraw(200)
+class Strings:
+    def getString(self):
+            self.text = input("Enter a string: ")
+    def printString(self):
+          print(self.text.upper())
+s = Strings()
+s.getString()
+s.printString()
